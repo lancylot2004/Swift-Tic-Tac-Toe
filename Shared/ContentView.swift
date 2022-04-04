dd0b1f6f8012e11ed8bd1f45eb1efa2485307d15
Hongming (Lancelot) Liu <lancylot2004@gmail.com>
2022-04-04 15:57:40 +0300

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by lancylot2004 on 04/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/Simple_TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_TicTacToeApp.swift
+//  Shared
+//
+//  Created by lancylot2004 on 04/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Simple_TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
