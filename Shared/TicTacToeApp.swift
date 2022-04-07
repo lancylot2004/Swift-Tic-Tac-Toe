1185dd55f3e24aa33f60336fbc9b429c3116731b
Hongming (Lancelot) Liu <lancylot2004@gmail.com>
2022-04-07 19:47:42 +0300

    Restricted iOS to portrait mode, enabled iPad and macOS versions, renamed main app to `TicTacToe`

--- Shared/TicTacToeApp.swift
+++ Shared/TicTacToeApp.swift
@@ -1,5 +1,5 @@
 //
-//  Simple_TicTacToeApp.swift
+//  TicTacToeApp.swift
 //  Shared
 //
 //  Created by lancylot2004 on 04/04/2022.
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct Simple_TicTacToeApp: App {
+struct TicTacToeApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
