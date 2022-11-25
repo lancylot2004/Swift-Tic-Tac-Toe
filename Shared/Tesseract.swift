ed242ad83d5dc65b40a6f1b1313d20127a16c6a2
Lancelot Liu <lancylot2004@proton.me>
2022-11-25 13:50:04 +0000

    Fixed issue where spamming option for computer player would caused unexpected behaviour, such as playing when board is locked, and incrementing score more than once.

--- Shared/Tesseract.swift
+++ Shared/Tesseract.swift
@@ -200,6 +200,7 @@ class Tesseract: ObservableObject {
     
     /// (*not*) ARTIFICIAL INTELLIGENCE
     func AIProcess() {
+        if locked == true { return }
         if AIPlayer == .none { return }
         if player != AIPlayer { return }
         
