cba35a4029c3365c152a7790560fc4bcbedabe81
Lancelot Liu <lancylot2004@proton.me>
2022-11-25 20:33:36 +0000

    Added window restrictions for macOS.

--- Shared/TicTacToeApp.swift
+++ Shared/TicTacToeApp.swift
@@ -10,12 +10,19 @@ import SwiftUI
 @main
 struct TicTacToeApp: App {
     var body: some Scene {
+        #if os(iOS)
         WindowGroup {
             MainView()
-                .fixedSize()
                 .if(UIDevice.current.userInterfaceIdiom == .pad) { mainView in
                     mainView.scaleEffect(1.3)
                 }
         }
+        #elseif os(macOS)
+        WindowGroup {
+            MainView()
+                .frame(width: Const.Dim.GameWidth, height: 4 * Const.Dim.GameSpacing + Const.Dim.GridSize + Const.Dim.LViewHeight + 2 * Const.Dim.SViewHeight)
+                .fixedSize()
+        }.windowStyle(.hiddenTitleBar)
+        #endif
     }
 }
